a9412626e58fae9dee9c1cce2d9e8b1518a47f01
Ingo Börner <ingoboerner@gmx.at>
2023-04-13 09:40:35 +0200

    implement function to map the dict keys of the metrics

--- corpus.py
+++ corpus.py
@@ -59,10 +59,13 @@ class Corpus:
         if acronym:
             self.acronym = acronym
 
-    def get_metrics(self) -> dict:
+    def get_metrics(self, use_mapping: bool = False) -> dict:
         """Assemble and return corpus metrics.
 
          Uses a SPARQL Query of class "CorpusMetrics" of the module "sparql_queries".
+
+        Args:
+            use_mapping (bool): Use a mapping to transform the keys of the metrics dictionary. Defaults to False
         """
 
         if self.metrics:
@@ -78,8 +81,43 @@ class Corpus:
                     results = query.results.simplify()
 
                     metrics = dict()
+
+                    if use_mapping:
+                        # map the keys of the results of the SPARQL query; unfortunately, this has to be hardcoded here;
+                        # Maybe the label could be included somewhere in the graph istead
+                        """
+                        'number_of_chapters': 700000,
+                        'number_of_characters': 20,
+                        'number_of_comments': 123,
+                        'number_of_documents': 200000,
+                        'number_of_female_characters': 8,
+                        'number_of_nonbinary_characters': 2,
+                        'number_of_paragraphs': 9000000,
+                        'number_of_words-comments': 123000,
+                        'number_of_words-text': 2000000000}
+                        """
+                        mapping = dict(
+                            number_of_chapters="chapters",
+                            number_of_characters="characters",
+                            number_of_comments="comments",
+                            number_of_documents="documents",
+                            number_of_female_characters="female",
+                            number_of_male_characters="male",
+                            number_of_nonbinary_characters="nonbinary",
+                            number_of_paragraphs="paragraphs"
+                        )
+                    else:
+                        mapping = None
+
                     for item in results:
-                        key = item["dimensionURI"].split("/")[-1:][0]
+                        key_from_graph = item["dimensionURI"].split("/")[-1:][0]
+                        if mapping:
+                            if key_from_graph in mapping:
+                                key = mapping[key_from_graph]
+                            else:
+                                key = key_from_graph
+                        else:
+                            key = key_from_graph
                         value = item["value"]
                         metrics[key] = value
 
@@ -166,6 +204,7 @@ class Corpus:
         )
 
         if include_metrics is True:
-            metadata["metrics"] = self.get_metrics()
+            # Use the hardcoded mappings by setting use_mapping to True
+            metadata["metrics"] = self.get_metrics(use_mapping=True)
 
         return metadata
