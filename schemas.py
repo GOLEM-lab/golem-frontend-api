4579ad4026efedaf4de942f69cd938cd6a8238d9
Ingo Börner <ingoboerner@gmx.at>
2023-04-14 10:42:02 +0200

    added schemas for characters and authors

--- schemas.py
+++ schemas.py
@@ -27,8 +27,12 @@ class CorpusMetrics(Schema):
     wordcount = fields.Nested(WordCount, required=True)
 
 
+# TODO: maybe merge schemas Corpus and CorpusMetadata with the one below; use optional fields
 class CorpusMetadata(Schema):
-    """Schema of the corpus metadata"""
+    """Schema of the corpus metadata
+
+    Included in the response of the /corpora endpoint
+    """
     name = fields.Str()
     uri = fields.Str()
     title = fields.Str()
@@ -36,3 +40,48 @@ class CorpusMetadata(Schema):
     description = fields.Str()
     metrics = fields.Nested(CorpusMetrics, required=False)
     repository = fields.Str()
+
+
+class ExternalReference(Schema):
+    """Item in External Reference Ressource"""
+    ref = fields.Str()
+    type = fields.Str()
+
+
+class Author(Schema):
+    """Metadata on a single author"""
+    id = fields.Str()
+    uri = field.Str() # maybe should add that
+    authorName = fields.Str()
+    authorFullname = fields.Str()
+    authorShortname = fields.Str()
+    refs = fields.Nested(ExternalReference)
+
+
+class Character(Schema):
+    """Metadata on a single character"""
+    id = fields.Str()
+    uri = fields.Str() # would add that
+    characterName = fields.Str()
+    entryName = fields.Str()
+    wikidataId = fields.Str() # Maybe harmonize with author (refs)?
+    source = fields.Str() # Maybe sourceName
+    sourceUrl = fields.Str
+    createdYear = fields.Int()
+    firstFanficYear = fields.Int()
+    numDocuments = fields.Int()
+    networkdataCsvUrl = fields.Str()
+    authors = fields.Nested(Author)
+
+class Corpus(Schema):
+    """Schema of the corpus.
+
+    Returned by the /corpora/{corpusname} endpoint
+    """
+    name = fields.Str()
+    uri = fields.Str()
+    title = fields.Str()
+    licence = fields.Str()
+    licenceUrl = fields.Str()
+    repository = fields.Str()
+    characters = fields.Nested(Character)
