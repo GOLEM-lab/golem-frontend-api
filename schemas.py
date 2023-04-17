2cc8ea94d342a6275762fa914e19002a4f75102a
Ingo Börner <ingoboerner@gmx.at>
2023-04-17 13:12:08 +0200

    fix errors in schemas

--- schemas.py
+++ schemas.py
@@ -1,20 +1,20 @@
 from marshmallow import Schema, fields
 
 
-class ApiInfo(Schema):
+class ApiInfoSchema(Schema):
     """Schema of the response of the 'api/info' endpoint"""
     name = fields.Str()
     version = fields.Str()
     description = fields.Str()
 
 
-class WordCount(Schema):
+class WordCountSchema(Schema):
     """Schema of the wordcount included in CorpusMetrics"""
-    words_in_documents = fields.Int()
-    words_in_comments = fields.Int()
+    wordsInDocuments = fields.Int()
+    wordsInComments = fields.Int()
 
 
-class CorpusMetrics(Schema):
+class CorpusMetricsSchema(Schema):
     """Schema of the corpus metrics included in the corpus metadata"""
     documents = fields.Int()
     chapters = fields.Int()
@@ -24,49 +24,49 @@ class CorpusMetrics(Schema):
     female = fields.Int()
     nonbinary = fields.Int()
     comments = fields.Int()
-    wordcount = fields.Nested(WordCount, required=True)
+    wordcount = fields.Nested(WordCountSchema, required=True)
 
 
-class ExternalReference(Schema):
+class ExternalReferenceSchema(Schema):
     """Item in External Reference Ressource"""
     ref = fields.Str()
     type = fields.Str()
 
 
-class Author(Schema):
+class AuthorSchema(Schema):
     """Metadata on a single author"""
     id = fields.Str()
     uri = fields.Str() # maybe should add that
     authorName = fields.Str()
-    refs = fields.Nested(ExternalReference)
+    refs = fields.Nested(ExternalReferenceSchema)
 
 
-class Character(Schema):
+class CharacterSchema(Schema):
     """Metadata on a single character"""
     id = fields.Str()
     uri = fields.Str() # would add that
     characterName = fields.Str()
     entryName = fields.Str()
-    refs = fields.Nested(ExternalReference, required=False)
-    source = fields.Str() # Maybe sourceName
+    refs = fields.Nested(ExternalReferenceSchema, required=False)
+    sourceName = fields.Str()
     sourceUrl = fields.Str
     createdYear = fields.Int()
     firstFanficYear = fields.Int()
     numDocuments = fields.Int()
-    networkdataCsvUrl = fields.Str()
-    authors = fields.Nested(Author)
+    characterCsvUrl = fields.Str()
+    authors = fields.Nested(AuthorSchema)
 
 
-class Corpus(Schema):
+class CorpusSchema(Schema):
     """Schema of the corpus.
     """
     id = fields.Str()
     uri = fields.Str()
     corpusName = fields.Str()
     acronym = fields.Str()
-    description = fields.Str(required=False)
+    corpusDescription = fields.Str(required=False)
     licence = fields.Str()
     licenceUrl = fields.Str()
     repository = fields.Str(required=False)
-    metrics = fields.Nested(CorpusMetrics, required=False)
-    characters = fields.Nested(Character, required=False)
+    metrics = fields.Nested(CorpusMetricsSchema, required=False)
+    characters = fields.Nested(CharacterSchema, required=False)
